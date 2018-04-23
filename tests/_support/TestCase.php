841067c8f6acc778a89b3f9dbda6e14f38c90447
Albert Borsos <albertborsos@me.com>
2018-04-23 17:52:24 +0200

    update tests for Request/Response interfaces
    #1

--- tests/_support/TestCase.php
+++ tests/_support/TestCase.php
@@ -79,9 +79,13 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
             ],
             'components' => [
                 'request' => [
+                    '__class' => \albertborsos\pjax\web\Request::class,
                     'cookieValidationKey' => 'wefJDF8sfdsfSDefwqdxj9oq',
                     'scriptFile' => __DIR__ . '/index.php',
                     'scriptUrl' => '/index.php',
+                ],
+                'response' => [
+                    '__class' => \albertborsos\pjax\web\Response::class,
                 ],
             ],
         ], $config));
